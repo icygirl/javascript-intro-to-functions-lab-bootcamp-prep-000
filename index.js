c14549c815e858e78673b4ccb77bec02cd0ff1eb
Virginia Herron <inadream@gmail.com>
2018-01-07 05:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function sayHiToGrandma(string) {
   else if (string.toUpperCase()) {
     return "YES INDEED!"
   }
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
